f6db4f9b2ed49e2c205ed72efdbe563d590ad706
Nagulmeera-1605 <2300032913@kluniversity.in>
2026-02-11 11:53:16 +0530

    Fix app.py syntax error

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Python App on Azure!"
+    return "Python App Successfully Deployed on Azure 🚀"
 
 if __name__ == "__main__":
     app.run()
